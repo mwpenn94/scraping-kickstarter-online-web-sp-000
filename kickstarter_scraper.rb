5c6c9902e3570c36ab9bc5ac1eff8a2b6e6955a9
Michael Penn <mwpenn94@gmail.com>
2021-03-15 23:09:28 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -13,13 +13,16 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   
   kickstarter = Nokogiri::HTML(html)
-  binding.pry
+  projects = {}
   
-    projects = {}
+    
     
  # Iterate through the projects
   kickstarter.css("li.project.grid_4").each do |project|
-    projects[project] = {}
+    title = project.css("h2.bbcard_name strong a").text
+    
+    projects[title.to_sym] = {}
+      :image_link => project.css("div.project-thumbnail a img").attribute("src").value
   end
  
   # return the projects hash
